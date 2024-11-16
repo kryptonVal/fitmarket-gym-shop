047dee542b15a5b4874af8e855b9264db54ec4ec
kryptonVal <valentine14okoye@gmail.com>
2024-11-16 13:52:01 +0200

    Update requirements and modify settings

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-
+from decouple import config
 from django.conf.global_settings import MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
